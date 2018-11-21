13f9bbb3893df1f98b786222158a6718a1434753
Tam Vu <vutam_a@hotmail.com>
2018-11-21 19:18:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.match(/^{aeiouAEIOU]+\w/)
+  if word.match(/^{aeiouAEIOU]+\w/)
+    true
+  else
+    false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
